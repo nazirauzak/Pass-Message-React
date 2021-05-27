6a2dd792fa4df84d3853a610890d06d406758cd2
Nazira Uzakbaeva <nananurtay@gmail.com>
2021-05-27 00:42:32 -0700

    fixed mistake

--- pass-message/src/App.js
+++ pass-message/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 // import 'styles.css';
 import './App.css';
-import Passmessage from './components/PassMessage'
+import PassMessage from './components/PassMessage'
 
-class  App extends React.Component {
+class App extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -12,42 +12,47 @@ class  App extends React.Component {
       alert: ""
     };
   }
+
   changed = (e) => {
-    this.setState({inputValue: e.target.value})
+    this.setState({ inputValue: e.target.value });
   };
+
   submitted = () => {
-    const { inputValue, alert} =this.state;
+    const { inputValue, alert } = this.state;
     if (inputValue) {
       this.setState({
         values: inputValue.toLowerCase(),
-        inputValue: ''
+        inputValue: ""
       });
-    }else {
-      this.setState({alert: "Please Enter A Value To Pass"});
-      setTimeout(() =>{
-        this.setState({alert: ''});
+    } else {
+      this.setState({ alert: "Please Enter A Value To Pass" });
+      setTimeout(() => {
+        this.setState({ alert: "" });
       }, 1500);
     }
-  }
+  };
   render() {
-    const { inputValue, values, alert} = this.state;
+    const { inputValue, values, alert } = this.state;
+
     return (
-      <div className='wrapper'>
-     <h3>A Message You Would Like To Pass</h3>
-      <div>
-        <input
-        type='text'
-        id='input'
-        value={inputValue}
-        onCahnge={this.changed}/>
-      <Passmessage function={this.submitted}/>
-      <p className='alert'>{alert}</p>
+      <div className="wrapper">
+        <h3>A Message You Would Like To Pass</h3>
+        <div>
+          <input
+            type="text"
+            id="input"
+            value={inputValue}
+            onChange={this.changed}
+          />
+          <PassMessage function={this.submitted} />
+          <p className="alert">{alert}</p>
+        </div>
+        <h3>Last Message Delivered</h3>
+        <p className="messages">{values}</p>
       </div>
-      <h3>Last Message Delivered</h3>
-      <p className= 'messages'>{values}</p>
-      </div>
-    )
+    );
   }
 }
 
 export default App;
+
